9d49c7f279a1a722dc823ab87fb660b3f0eec7a0
Declan Murphy <declanmurphy@Declans-MacBook-Air-2.local>
2025-05-17 14:49:33 -0400

    Create initial script file

---
+++ Median_Transport_Price.R
@@ -0,0 +1,10 @@
+##### Median Transportation Prices by Geography #####
+
+# Raw R Script to load and tranform data
+# For analysis and insights, see the RMarkdown file / HTML output
+
+# Setup
+
+install.packages("requiRements")
+library(requiRements)
+library(tidycensus)
